8d92157a61dc8eae5f32459a05fca3fd5376bdfe
mmpowers <michael@mpowers.net>
2025-06-28 15:58:58 -0400

    Updated schedule. Fixed onclick issues on ios.

--- e2e/pages/ContactUser.js
+++ e2e/pages/ContactUser.js
@@ -1,17 +1,17 @@
-var ContactUser = function() {
+var ContactUser = function () {
 
-  this.load = function() {
+  this.load = function () {
     return browser.get("/#");
   };
 
-  this.buttons = function() {
+  this.buttons = function () {
     // @Todo - remove sleep() when https://github.com/angular/protractor/issues/2154
     browser.sleep(500);
 
-    return element.all(by.css('[ng-click="cp.submitContact(item)"]'));
+    return element.all(by.css('[ng-mousedown="cp.submitContact(item)"]'));
   };
 
-  this.focusedButton = function() {
+  this.focusedButton = function () {
     return browser.driver.switchTo().activeElement();
   };
 
